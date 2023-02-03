7999500effe9438d88ab412e36eacd8e5acd4412
Sergei Chernov <sergey.v.chernov@yandex.ru>
2023-02-03 20:39:19 +0500

    Инструменты [#504887 #387653]

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -4,8 +4,6 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 /**
  * Unit test for simple App.
  */
