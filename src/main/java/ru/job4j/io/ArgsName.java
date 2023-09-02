5eafb3e2d66456c46e315d848bdb3bbffd061e22
Sergei Chernov <sergey.v.chernov@yandex.ru>
2023-09-02 12:31:36 +0600

    13. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,76 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.String;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        inputCheck(key);
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        String[] parameter;
+
+        for (int i = 0; i < args.length; i++) {
+            parameter = args[i].split("=", 2);
+            values.put(parameter[0].substring(1), parameter[1]);
+        }
+    }
+
+    private void inputCheck(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Error: Key shouldn't equal to zero");
+        }
+        if (values.get(key) == null) {
+            String error = String.format("This key: '%s' is missing", key);
+            throw new IllegalArgumentException(error);
+        }
+    }
+
+    private void argsCheck(String[] args) {
+
+        char equal = 61;
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Arguments not passed to program");
+        }
+
+        for (int i = 0; i < args.length; i++) {
+
+            if (!args[i].contains("=")) {
+                String error = String.format("Error: This argument '%s' does not contain an equal sign", args[i]);
+                throw new IllegalArgumentException(error);
+            }
+            if (!args[i].startsWith("-")) {
+                String error = String.format("Error: This argument '%s' does not start with a '-' character", args[i]);
+                throw new IllegalArgumentException(error);
+            }
+            if (args[i].startsWith("-") && args[i].charAt(1) == equal) {
+                String error = String.format("Error: This argument '%s' does not contain a key", args[i]);
+                throw new IllegalArgumentException(error);
+            }
+            if (args[i].split("=", 2)[1].length() < 1) {
+                String error = String.format("Error: This argument '%s' does not contain a value", args[i]);
+                throw new IllegalArgumentException(error);
+            }
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.argsCheck(args);
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-?=="});
+        System.out.println(jvm.get("?"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
