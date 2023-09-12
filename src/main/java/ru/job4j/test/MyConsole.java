b8c79a0e77ba24908f63d26d43050873f7a33891
Sergei Chernov <sergey.v.chernov@yandex.ru>
2023-09-12 16:58:25 +0600

    18. Класс Console [#504991]

---
+++ src/main/java/ru/job4j/test/MyConsole.java
@@ -0,0 +1,22 @@
+package ru.job4j.test;
+
+ import java.io.Console;
+import java.util.Arrays;
+
+public class MyConsole {
+
+    public static void main(String[] args) {
+        String login;
+        char[] charPassword;
+        Console console = System.console();
+        if (console == null) {
+            System.out.println("Console is unavailable.");
+            return;
+        }
+        login = console.readLine("%s", "Enter login: ");
+        console.printf("Your login: %s\n", login);
+        charPassword = console.readPassword("%s", "Enter your password: ");
+        System.out.println("Your pass: " + String.valueOf(charPassword));
+        Arrays.fill(charPassword, ' ');
+    }
+}
