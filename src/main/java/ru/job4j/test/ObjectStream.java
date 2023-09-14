8d230490af2e84bac4e58d56eb952a761fadf313
Sergei Chernov <sergey.v.chernov@yandex.ru>
2023-09-14 18:31:59 +0600

    21. Потоки объектов. (Object streams) [#504992]

---
+++ src/main/java/ru/job4j/test/ObjectStream.java
@@ -0,0 +1,17 @@
+package ru.job4j.test;
+
+import java.io.*;
+
+public class ObjectStream {
+    public static void main(String[] args) {
+        Car car = new Car("Company", "Model", 2000);
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("C:\\projects\\Test\\serialized.dat"));
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream("C:\\projects\\Test\\serialized.dat"))) {
+            out.writeObject(car);
+            Car deserialized = (Car) in.readObject();
+            System.out.println(deserialized.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
